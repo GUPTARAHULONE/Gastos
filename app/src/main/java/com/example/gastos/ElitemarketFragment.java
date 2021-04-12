8141eaac4f8ca0c7248db7a6e09155ca8ce77fd0
arpitsaxena47 <51399299+arpitsaxena47@users.noreply.github.com>
2021-04-12 21:01:48 +0530

    scrollview added

--- app/src/main/java/com/example/gastos/ElitemarketFragment.java
+++ app/src/main/java/com/example/gastos/ElitemarketFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class ElitemarketFragment extends Fragment {
@@ -35,6 +36,13 @@ public class ElitemarketFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_elitemarket, container, false);
+        View view =  inflater.inflate(R.layout.fragment_elitemarket, container, false);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Elite Activity", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return  view;
     }
 }
