db6b47d4a55b37b7faf1d89a14a6d9d902980cf2
GUPTARAHULONE <guptarahul26318@gmail.com>
2021-04-13 18:03:46 +0530

    update

--- app/src/main/java/com/example/gastos/DealsFragment.java
+++ app/src/main/java/com/example/gastos/DealsFragment.java
@@ -3,15 +3,21 @@ package com.example.gastos;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DealsFragment extends Fragment {
 
-
+    RecyclerView recyclerView;
+    List<Model_order> orderlist;
     public DealsFragment() {
         // Required empty public constructor
     }
@@ -34,6 +40,23 @@ public class DealsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deals, container, false);
+        View view= inflater.inflate(R.layout.fragment_deals, container, false);
+
+       recyclerView =view.findViewById(R.id.offer);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        //initdata();
+        recyclerView.setAdapter(new orderAdapter(initdata()));
+               return view;
+    }
+
+    private List<Model_order> initdata() {
+         orderlist=new ArrayList<Model_order>();
+         orderlist.add(new Model_order("All"));
+        orderlist.add(new Model_order("Shopping"));
+        orderlist.add(new Model_order("Health"));
+        orderlist.add(new Model_order("Trending Offers"));
+        return orderlist;
     }
 }
