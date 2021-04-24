56353d705d077808f8724e73399c65046361bd6d
GUPTARAHULONE <guptarahul26318@gmail.com>
2021-04-24 10:52:51 +0530

    local Fragment update

---
+++ app/src/main/java/com/example/gastos/User_Information_Activity.java
@@ -0,0 +1,14 @@
+package com.example.gastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Information_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__information_);
+    }
+}
