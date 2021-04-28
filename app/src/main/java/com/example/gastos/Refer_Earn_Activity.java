76a596ef7349a36b94e034e427388d09073880d3
arpitsaxena47 <51399299+arpitsaxena47@users.noreply.github.com>
2021-04-28 12:43:03 +0530

    refer and earn update

--- app/src/main/java/com/example/gastos/Refer_Earn_Activity.java
+++ app/src/main/java/com/example/gastos/Refer_Earn_Activity.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Refer_Earn_Activity extends AppCompatActivity {
-    Button popup1;
+public class Refer_Earn_Activity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton drop1,drop2,drop3;
+    TextView txtDrop1,txtDrop2,txtDrop3;
 
 
     @Override
@@ -31,7 +34,17 @@ public class Refer_Earn_Activity extends AppCompatActivity {
         editTextFilledExposedDropdown.setAdapter(adapter);
 
       */
-        popup1 = findViewById(R.id.pop1);
+       drop1 = findViewById(R.id.btn_drop1);
+       drop2 = findViewById(R.id.btn_drop2);
+       drop3 = findViewById(R.id.btn_drop3);
+
+       drop1.setOnClickListener(this);
+       drop2.setOnClickListener(this);
+       drop3.setOnClickListener(this);
+
+       txtDrop1 = findViewById(R.id.txtDrop1);
+       txtDrop2 = findViewById(R.id.txtDrop2);
+       txtDrop3 = findViewById(R.id.txtDrop3);
         //  public void showpopup1(View v)
         //   {
 
@@ -51,5 +64,32 @@ public class Refer_Earn_Activity extends AppCompatActivity {
     }
 
         */
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.btn_drop1:
+                if(txtDrop1.getVisibility() == View.GONE)
+                txtDrop1.setVisibility(View.VISIBLE);
+                else
+                txtDrop1.setVisibility(View.GONE);
+                break;
+
+            case R.id.btn_drop2:
+                if(txtDrop2.getVisibility() == View.GONE)
+                    txtDrop2.setVisibility(View.VISIBLE);
+                else
+                    txtDrop2.setVisibility(View.GONE);
+                break;
+
+            case R.id.btn_drop3:
+                if(txtDrop3.getVisibility() == View.GONE)
+                    txtDrop3.setVisibility(View.VISIBLE);
+                else
+                    txtDrop3.setVisibility(View.GONE);
+                break;
+        }
     }
 }
